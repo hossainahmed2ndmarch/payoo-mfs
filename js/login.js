f9426329344d4e270da90010e037008bbc9c3391
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2024-09-20 09:32:29 +0600

    Added the add money form

--- js/login.js
+++ js/login.js
@@ -1,4 +1,14 @@
 document.getElementById('button-login')
- .addEventListener('click', function () {
+ .addEventListener('click', function (event) {
+  event.preventDefault();
   console.log('Login Button clicked');
+  const phoneNumber = document.getElementById('phn-nmbr').value;
+  const pinNumber = document.getElementById('pin-nmbr').value;
+  if (phoneNumber === '3' && pinNumber === '1234') {
+   console.log('You are logged in');
+   window.location.href = '/home.html';
+  }
+  else {
+   alert('Incorrect phone or pin');
+  }
  })
